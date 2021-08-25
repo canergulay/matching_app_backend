7e8dbcab0a64205f5c5f3a7a46ea1fc71e27550e
Caner Gülay <caner.gulay@hotmail.com>
2021-08-25 09:59:41 +0300

    new features was corrected

--- src/utils/code/cache_controller.js
+++
@@ -1,8 +0,0 @@
-const User = require('../../models/user')
-
-async function cacheController(mail){
-    const user = await User.findOne({mail:mail})
-    return user
-}
-
-module.exports = cacheController
--- src/utils/code/email_cacher.js
+++
@@ -1,8 +0,0 @@
-const User = require('../../models/user')
-
-async function mailCacher(mail){
-    const mailToBeCached = new User({mail:mail})
-    await mailToBeCached.save()
-}
-
-module.exports = mailCacher
